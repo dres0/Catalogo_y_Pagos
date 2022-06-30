75eaf895a55b6e82f07a4aa33cd5d8ef5008bb8c
dres0 <humberto.ferreiray@gmail.com>
2022-06-30 00:23:59 -0400

    Product and Category models done

---
+++ db/migrate/20220630042326_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.integer :stock
+      t.decimal :price
+      t.string :image
+      t.string :sku
+
+      t.timestamps
+    end
+  end
+end
